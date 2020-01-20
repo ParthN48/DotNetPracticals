74d6a65d71804e090c4ce9a7ffaaad25da2995ba
parthnonghanvadra <parthnonghanvadra@gmail.com>
2020-01-20 13:48:48 +0530

    Regex included

--- Practical2/Program.cs
+++ Practical2/Program.cs
@@ -1,18 +1,27 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Practical2
 {
     class Program
     {
+        private const string V = "^[a-zA-Z]+$";
+
         static void Main(string[] args)
         {
             String name;
             String country;
-            Console.WriteLine("Please Enter your Name: ");
-            name = Console.ReadLine();
-            Console.WriteLine("Please Enter your Country: ");
-            country = Console.ReadLine();
-            Console.WriteLine("Hi, " + name + " You are from " + country);
+            do{
+                Console.WriteLine("Please Enter your Name: ");
+                name = Console.ReadLine();
+            }while(!Regex.IsMatch(name,"^[a-zA-Z]+$"));
+
+            do{
+                Console.WriteLine("Please Enter country: ");
+                country = Console.ReadLine();
+            }while(!Regex.IsMatch(country,"^[a-zA-Z]+$"));
+
+            Console.WriteLine("Hi, " + name + " ,from " + country);
             Console.ReadLine();
         }
     }
