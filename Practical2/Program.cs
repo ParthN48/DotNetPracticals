bd6b49b980e464727729603890e750dba41db818
parthnonghanvadra <parthnonghanvadra@gmail.com>
2020-01-20 12:49:09 +0530

    initial Commit

---
+++ Practical2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Practical2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name;
+            String country;
+            Console.WriteLine("Please Enter your Name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Please Enter your Country: ");
+            country = Console.ReadLine();
+            Console.WriteLine("Hi, " + name + " You are from " + country);
+            Console.ReadLine();
+        }
+    }
+}
